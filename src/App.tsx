1c73c4efa7326d0e57f4b146f2a753fbdf9c2a9f
npd29 <npd29@comcast.net>
2024-08-01 16:41:55 -0400

    [build] - Remove CRA and switch to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     return (
         <div className="App">
             <Hero></Hero>
-            <SkillsSection></SkillsSection>
+            {/* <SkillsSection></SkillsSection> */}
             <About></About>
             <Projects></Projects>
             <Contact></Contact>
--- src/components/skills/skillIcon.tsx
+++ src/components/skills/skillIcon.tsx
@@ -1,4 +1,4 @@
-import {
+import React, {
     lazy,
     LazyExoticComponent,
     Suspense,
@@ -6,55 +6,36 @@ import {
     useState
 } from 'react';
 import { Skill } from '../../types';
-import React from 'react';
-import { listeners } from 'process';
 
 type SkillProps = {
-    skillKey: any;
+    skillKey: string;
     skill: Skill;
 };
+
 const importSkillComponent = (
     key: string
 ): LazyExoticComponent<React.FC<any>> => {
-    return lazy(() =>
-        import(`../../components/icons/${key}Icon`).then((module) => ({
-            default: module[`${key.charAt(0).toUpperCase() + key.slice(1)}Icon`]
-        }))
-    );
+    return lazy(() => import(`../../components/icons/${key}.svg`));
 };
 
 export function SkillIcon({ skillKey, skill }: SkillProps) {
-    const [hovering, setHovering] = useState(false);
     const [SkillComponent, setSkillComponent] = useState<LazyExoticComponent<
         React.FC<any>
     > | null>(null);
+
     useEffect(() => {
-        const loadComponent = async () => {
+        const loadComponent = () => {
             const component = importSkillComponent(skillKey);
             setSkillComponent(() => component);
         };
         loadComponent();
     }, [skillKey]);
-    return (
-        <div
-            onMouseEnter={() => setHovering(true)}
-            onMouseLeave={() => setHovering(false)}
-            className="skill"
-            key={skillKey}
-        >
-            <Suspense fallback={<div>Loading...</div>}>
-                {SkillComponent && (
-                    <SkillComponent
-                        light1="#aaa"
-                        dark1="#888"
-                        light2="#aaa"
-                        dark2="#888"
-                        background="#fff"
-                        hovering={hovering} // Adjust this state as needed
-                    />
-                )}
-            </Suspense>
 
+    return (
+        <div className="skill" key={skillKey}>
+            <Suspense fallback={<div>Loading...</div>}>
+                {SkillComponent && <SkillComponent />}
+            </Suspense>
             <h3>{skill.name}</h3>
         </div>
     );
