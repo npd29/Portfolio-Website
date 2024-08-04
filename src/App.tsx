7ca2c6b1cd3fd960516ca49f205b59a47b5b3fb0
npd29 <npd29@comcast.net>
2024-08-03 21:40:35 -0400

    [feat] - Add disclaimer banner

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { Contact } from './components/contact';
 import { Hero } from './components/hero/hero';
 import { Footer } from './components/footer/footer';
 import SkillsSection from './components/skills/skillsSection';
+import Banner from './components/banner/banner';
 
 function App() {
     return (
         <div className="App">
+            <Banner></Banner>
             <Hero></Hero>
             <About></About>
             <SkillsSection></SkillsSection>
---
+++ src/components/banner/banner.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './banner.scss';
+import { CloseIcon } from '../icons/close';
+import { Button } from '@mui/material';
+export default function Banner() {
+    const [showBanner, setShowBannner] = React.useState(true);
+    return (
+        showBanner && (
+            <div className="banner">
+                <p>
+                    <span>Note:</span> This site is still under constuction
+                    while transistioning from the{' '}
+                    <a href="https://ndesmara.w3.uvm.edu/portfolio/?id=new-site">
+                        original
+                    </a>
+                    .
+                </p>
+                <Button onClick={() => setShowBannner(false)}>
+                    <CloseIcon color="#FFFFFF" />
+                </Button>
+            </div>
+        )
+    );
+}
