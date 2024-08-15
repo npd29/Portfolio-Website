f91c36fbc5653bb48850fcb6993fed4f03e0919b
npd29 <npd29@comcast.net>
2024-08-15 19:31:27 -0400

    [fix] - Remove x86 from project

--- src/store.ts
+++ src/store.ts
@@ -92,7 +92,7 @@ export const projects: Project[] = [
         name: 'Custom Compiler',
         description:
             "For my final project in the course on compiler design and construction, I focused on extending the language's capabilities by implementing simple dataclasses. This project was a culmination of the various topics covered throughout the course, which provided a comprehensive understanding of the compilation process.\r\nThroughout the semester, I explored the representation and analysis of code and how to translate a high-level programming language into Intel x86 assembly language. We learned about essential concepts such as register allocation, static type checking, and handling mutable data. The course also emphasized the significance of garbage collection for efficient memory management in compiled languages.\r\nBy extending the language with dataclasses, I not only demonstrated my proficiency in implementing new language features but also showcased my grasp of the entire compilation process. This project solidified my knowledge of code analysis, code generation, and the crucial role of language features in creating powerful and expressive programming languages.",
-        languages: ['python', 'x86'],
+        languages: ['python'],
         tools: [],
         date: undefined,
         githubLink: 'https://github.com/npd29/CS202-Final',
