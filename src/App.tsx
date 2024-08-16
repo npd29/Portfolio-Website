4e2e91869afefca36ac6d9e97e89ab7dff747d9f
npd29 <npd29@comcast.net>
2024-08-16 19:42:35 -0400

    [feat] - Begin hooking up Azure functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { About } from './components/about/about';
 import { Contact } from './components/contact';
 import { Hero } from './components/hero/hero';
 import { Footer } from './components/footer/footer';
-import SkillsSection from './components/skills/skillsSection';
 import Banner from './components/banner/banner';
 
 function App() {
