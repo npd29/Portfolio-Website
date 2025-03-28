b08577d6489c315cd2eed5fa8716bcfaad3cda69
npd29 <npd29@comcast.net>
2025-03-28 17:44:27 -0400

    [fix] - Update about section content

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -10,48 +10,79 @@ export function About() {
                 <div className="about-container">
                     <div className="about-text">
                         <p>
-                            I am a passionate and versatile software engineer
-                            with a degree in Computer Science &amp; Information
-                            Systems from the University of Vermont. I have a
-                            strong foundation in C++, Java, Python, which I
-                            gained while working as an Advanced Programming
-                            Teaching Assistant. There, I also taught and
-                            assisted students with the implementation and
-                            understanding of data structures and algorithms. I
-                            currently work at Trinnex, a CDM Smith subsidiary as
-                            a software developer. There I have been working on
-                            creating complex web-based applications using
-                            Typescript, React, and implementing Azure functions.
+                            I'm a passionate and versatile software engineer
+                            with a degree in Computer Science & Information
+                            Systems from the University of Vermont. My journey
+                            in software development has been fueled by a love of
+                            problem solving, constant curiosity, and a drive to
+                            continuously expand my skill set.
                         </p>
+
                         <p>
-                            In addition to my professional work, I have been
-                            involved in academic and personal projects that
-                            showcase my creativity and problem-solving skills.
-                            One of my most recent projects is an LED matrix
-                            inspired by the{' '}
-                            <a href="https://tidbyt.com">TidByt</a> using
-                            Raspberry Pi Pico and CircuitPython. I am in the
-                            process of creating my own LED Matrix layout library
-                            in C++ to have more control over memory management
-                            and speed.
+                            I started my programming journey with C++, Java, and
+                            Python in college, building a strong foundation.
+                            Then, as an Advanced Programming Teaching Assistant,
+                            I honed these skills further by helping students
+                            debug their code and understand complex concepts in
+                            data structures and algorithms. This experience not
+                            only strengthened my technical abilities but also
+                            improved my ability to communicate technical topics
+                            at various levels. My interest in AI and ML began in
+                            an evolutionary robotics course, where I developed a
+                            simulated robot using evolutionary algorithms to
+                            navigate obstacles. This sparked a lasting
+                            fascination with AI, which I’ve continued to
+                            explore.
                         </p>
+
                         <p>
-                            My passion lies in AI technologies and their
-                            applications in various domains. In my free time, I
-                            enjoy working on projects and online courses
-                            pertaining to the subject, my latest being a Snake
-                            AI using reinforcement learning. These projects help
-                            me apply my theoretical knowledge and skills to
-                            practical challenges. I am also in the process of
-                            obtaining my AWS Certified Solutions Architect
-                            Associate Certification to expand my knowledge on
-                            cloud computing and AI.
+                            Currently, I’m a Software Developer at Trinnex, a
+                            subsidiary of CDM Smith, where I develop full-stack
+                            web applications. I focus on creating scalable,
+                            reusable components, designing APIs, optimizing
+                            database queries, and ensuring seamless integration
+                            between services. One of my most impactful projects
+                            was a $2.2M Kansas DOT contract, where I transformed
+                            large datasets into digestible dashboards. I also
+                            collaborate closely with UX and Product teams to
+                            implement user-focused features that enhance data
+                            interpretation and usability. My passion for AI led
+                            me to seek mentorship from the Data Science team,
+                            where I’ve taken on projects and online courses to
+                            deepen my expertise in this area.
                         </p>
+
                         <p>
-                            I am a lifelong learner, curious by nature, and a
-                            self-starter, always looking for new challenges and
-                            opportunities to grow. To learn more about my
-                            projects, please continue reading below.
+                            Outside of work, I love taking on personal projects
+                            that blend creativity with technical
+                            problem-solving. One of my recent projects is an
+                            IoT-based environmental monitoring display, inspired
+                            by the Tidbyt, built using a Raspberry Pi Pico W and
+                            Python. I’m also developing my own layout library in
+                            C++ to optimize performance, giving me more control
+                            over memory management and execution speed.
+                        </p>
+
+                        <p>
+                            I strive to be a well-rounded developer with
+                            expertise in full-stack development, AI, and cloud
+                            computing. I’m particularly intrigued by the
+                            evolving role of software developers as AI advances,
+                            and how we can leverage AI as a tool alongside
+                            domain expertise to create impactful, scalable
+                            solutions. My goal is to bridge the gap between
+                            front-end and back-end development, infrastructure,
+                            and emerging technologies.
+                        </p>
+
+                        <p>
+                            I'm a lifelong learner, always seeking out new
+                            challenges that push me to grow as an engineer. The
+                            excitement of discovering new solutions and pushing
+                            the boundaries of what's possible keeps me energized
+                            and eager to see what challenges tomorrow brings. If
+                            you're interested in my work, feel free to explore
+                            my projects below.
                         </p>
                     </div>
                     <div className="image-container">
